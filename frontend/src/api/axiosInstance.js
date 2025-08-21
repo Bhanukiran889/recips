864e72cf3551fc6714c11313998a3928709f967f
Bhanukiran Reddy <bkreddy889@gmail.com>
2025-08-21 21:52:13 +0530

    last commit

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000", // replace with your backend URL
+  baseURL: "https://recips.onrender.com", // replace with your backend URL
   headers: {
     "Content-Type": "application/json",
   },
